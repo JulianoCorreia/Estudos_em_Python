f13d0aa68c54086a95bd346d42e25fd3b94841e0
JulianoCorreia <47059664+JulianoCorreia@users.noreply.github.com>
2023-01-16 23:11:31 -0300

    Update Desafio035.py

--- Modulo01/Desafios/Desafio035.py
+++ Modulo01/Desafios/Desafio035.py
@@ -3,12 +3,17 @@ Desenvolva um programa que leia o comprimento de três retas e
 diga ao usuário se elas podem ou não formar um triângulo.
 '''
 
+from time import sleep
 print('=====   DESAFIO 035   =====')
 print(" ")
-from time import sleep
 
 a = float(input('Primeiro segmento: '))
 b = float(input('Segundo segmento: '))
 c = float(input('Terceiro segmento: '))
 print('Analisando o triângulo...')
 sleep(2)
+
+if a < b + c and b < a + c and c < a + b:
+    print('Os segmentos acima PODEM FORMAR um triângulo.')
+else:
+    print('Os segmentos acima NÃO PODEM um triângulo.')
